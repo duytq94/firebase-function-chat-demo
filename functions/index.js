cffa061159e0bd34c2c89d5ce2fda527692f4958
Duy Tran <tranquangduy7994@gmail.com>
2019-07-09 16:42:51 +0700

    init new code

--- functions/index.js
+++ functions/index.js
@@ -3,38 +3,56 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 
 exports.sendNotification = functions.firestore
-	.document('messages/{groupId1}/{groupId2}/{message}')
-	.onCreate((snap, context) => {
-		console.log('----------------start function--------------------')
+  .document('messages/{groupId1}/{groupId2}/{message}')
+  .onCreate((snap, context) => {
+    console.log('----------------start function--------------------')
 
-		const idFrom = snap.data().idFrom
-		const idTo = snap.data().idTo
-		const content = snap.data().content
+    const idFrom = snap.data().idFrom
+    const idTo = snap.data().idTo
+    const content = snap.data().content
 
-		admin
-			.firestore()
-			.collection('users')
-			.where('id', '==', idTo)
-			.get()
-			.then(querySnapshot => {
-				querySnapshot.forEach(documentSnapshot => {
-					console.log(`Push notification to ${documentSnapshot.data().nickname}`)
-					if (documentSnapshot.data().pushToken && !documentSnapshot.data().isOnline) {
-						const payload = {
-							notification: {
-								title: `You have a message from ${idFrom}`,
-								body: content,
-								badge: '1',
-								sound: 'default',
-							},
-						}
+    // Get push token user to (receive)
+    admin
+      .firestore()
+      .collection('users')
+      .where('id', '==', idTo)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(infoUserTo => {
+          console.log(`Found user to: ${infoUserTo.data().nickname}`)
+          if (infoUserTo.data().pushToken) {
+            // Get info user from (sent)
+            admin
+              .firestore()
+              .collection('users')
+              .where('id', '==', idFrom)
+              .get()
+              .then(querySnapshot2 => {
+                querySnapshot2.forEach(infoUserFrom => {
+                  console.log(
+                    `Found user from: ${infoUserFrom.data().nickname}`
+                  )
+                  const payload = {
+                    notification: {
+                      title: `You have a message from "${
+                        infoUserFrom.data().nickname
+                      }"`,
+                      body: content,
+                      badge: '1',
+                      sound: 'default'
+                    }
+                  }
+                  admin
+                    .messaging()
+                    .sendToDevice(infoUserTo.data().pushToken, payload)
+                  console.log(`Notification is sent with content "${content}"`)
+                })
+              })
+          } else {
+            console.log('Can not find pushToken target user')
+          }
+        })
+      })
 
-						admin.messaging().sendToDevice(documentSnapshot.data().pushToken, payload)
-					} else {
-						console.log('Can not find pushToken target user')
-					}
-				})
-
-				console.log('----------------end function--------------------')
-			})
-	})
+    return null
+  })
